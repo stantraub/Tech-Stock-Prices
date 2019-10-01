01aabc52a48de2c586169727d05072c4ae99b0c1
Stanley <stanley.traub@gmail.com>
2019-10-01 15:48:37 -0700

    Fixed charts

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,6 @@
 
 const api = 'https://cloud.iexapis.com/stable/stock/aapl/chart/5d?token=pk_dfb132b12db14003bfeb90dc058b276c&chartCloseOnly=true';
-
+// const api = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=2017-12-31&end=2018-04-01';
 document.addEventListener("DOMContentLoaded", function (event) {
     fetch(api)
         .then(function (response) { return response.json(); })
@@ -13,16 +13,26 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
 function parseData(data) {
     let arr = [];
-    for (let i in data.close) {
-        arr.push({ date: new Date(i), //date            
-            value: +data.close[i] //convert string to number         
+    console.log(data);
+    for (let i = 0; i < data.length; i++) {
+        arr.push({ date: new Date(data[i].date), //date            
+            close: data[i].close //convert string to number         
         });   
     }   
     return arr;
 }
 
+// function parseData(data) {
+//     var arr = []; for (var i in data.bpi) {
+//         arr.push({
+//             date: new Date(i), //date            
+//             value: +data.bpi[i] //convert string to number
+//                  });   }   return arr;}
+
+
 function drawChart(data) {
-    let svgWidth = 600, svgHeight = 400;
+    console.log(data);
+    let svgWidth = 1000, svgHeight = 600;
     let margin = { top: 20, right: 20, bottom: 30, left: 50 };
     let width = svgWidth - margin.left - margin.right; 
     let height = svgHeight - margin.top - margin.bottom;
@@ -39,11 +49,29 @@ function drawChart(data) {
     let y = d3.scaleLinear()
         .rangeRound([height, 0]);
 
+    
+    // let line = d3.line()
+    // .x(function (d) {
+    //     console.log(d);
+    //     return x(d.date) 
+
+    // })
+
+    
+    // .y(function (d) { return y(d.value) })
+    //    x.domain(d3.extent(data, function (d) { return d.date })); y.domain(d3.extent(data, function (d) { return d.value }));
+
+
     let line = d3.line()
-        .x(function (d) { return x(d.date) })
-        .y(function (d) { return y(d.value) })   
+        .x(function (d) {
+            console.log(d);
+            { return x(d.date) }
+        }
+        
+        )
+        .y(function (d) { return y(d.close) })   
         x.domain(d3.extent(data, function (d) { return d.date })); 
-        y.domain(d3.extent(data, function (d) { return d.value }));
+        y.domain(d3.extent(data, function (d) { return d.close }));
     
     g.append("g")
         .attr("transform", "translate(0," + height + ")")
@@ -66,7 +94,9 @@ function drawChart(data) {
         .attr("fill", "none")
         .attr("stroke", "steelblue")
         .attr("stroke-linejoin", "round")
-        .attr("stroke-linecap", "round").attr("stroke-width", 1.5).attr("d", line);
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 1.5)
+        .attr("d", line);
 
 
 
