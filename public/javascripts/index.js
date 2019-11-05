c01efc5ed49584a174e58b4f8e6548ccb1f23a80
stanley.traub@gmail.com <stanley.traub@gmail.com>
2019-11-05 09:42:25 -0800

    Fixed index.js

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,4 +1,5 @@
 let symbol = 'aapl';
+import Chart from 'chart.js';
 let interval = '5d'
 let chart = `https://cloud.iexapis.com/stable/stock/${symbol}/chart/${interval}?token=pk_dfb132b12db14003bfeb90dc058b276c&chartCloseOnly=true`;
 let info = `https://cloud.iexapis.com/stable/stock/${symbol}/company?token=pk_dfb132b12db14003bfeb90dc058b276c&format=json`;
@@ -7,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     fetch(chart)
         .then(function (response) { return response.json(); })
         .then(function (data) {
-                   //DO SOMETHING WITH DATA      
+            //DO SOMETHING WITH DATA      
             let parsedData = parseData(data);
             drawChart(parsedData);
         });
@@ -16,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
         .then(function (response) { return response.json(); })
         .then(function (data) {
             //DO SOMETHING WITH DATA      
-        
+
             parseCompanyData(data);
         });
 
@@ -25,7 +26,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     dropval.onchange = function () {
         symbol = document.getElementById('dropval').value;
         // console.log(symbol);
-        
+
         chart = `https://cloud.iexapis.com/stable/stock/${symbol}/chart/5d?token=pk_dfb132b12db14003bfeb90dc058b276c&chartCloseOnly=true`;
         info = `https://cloud.iexapis.com/stable/stock/${symbol}/company?token=pk_dfb132b12db14003bfeb90dc058b276c&format=json`;
         // console.log(chart);
@@ -34,7 +35,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
             .then(function (data) {
                 //DO SOMETHING WITH DATA      
                 let parsedData = parseData(data);
-             
+                
                 drawChart(parsedData);
             });
 
@@ -46,32 +47,32 @@ document.addEventListener("DOMContentLoaded", function (event) {
                 parseCompanyData(data);
             });
 
-        
+
 
     }
 
-    
+
 });
 
 function parseData(data) {
     let arr = [];
     // console.log(data);
     for (let i = 0; i < data.length; i++) {
-        arr.push({ 
+        arr.push({
             date: new Date(data[i].date), //date            
             close: data[i].close //convert string to number         
-        });   
+        });
 
-    }   
+    }
 
     const closing = document.getElementById('closing');
     const change = document.getElementById('change');
     const percentage = document.getElementById('percentage');
- 
+
 
     let last = arr[arr.length - 1];
-    
-    let difference = Math.round((last.close - arr[arr.length - 2].close)* 100)/100;
+
+    let difference = Math.round((last.close - arr[arr.length - 2].close) * 100) / 100;
     let difpercent = Math.round(((last.close - arr[arr.length - 2].close) / arr[arr.length - 2].close) * 100) / 100;
     // rounded = Math.round(difference * 100)/100;
     // let difference = 5.60
@@ -94,125 +95,98 @@ function parseData(data) {
     }
     return arr;
 
-    
+
 }
 
 function drawChart(data) {
-    let svgWidth = 600, svgHeight = 330;
-    let margin = { top: 20, right: 20, bottom: 30, left: 50 };
-    let width = svgWidth - margin.left - margin.right; 
-    let height = svgHeight - margin.top - margin.bottom;
-
-    // find data range
-    const xMin = d3.min(data, d => {
-        return d['date'];
-    });
-    const xMax = d3.max(data, d => {
-        return d['date'];
-    });
-    const yMin = d3.min(data, d => {
-        return d['close'];
-    });
-    const yMax = d3.max(data, d => {
-        return d['close'];
-    });
-    // scales for the charts
-    const xScale = d3
-        .scaleTime()
-        .domain([xMin, xMax])
-        .range([0, width]);
-    const yScale = d3
-        .scaleLinear()
-        .domain([yMin - 5, yMax])
-        .range([height, 0]);
-
-    d3.select('#svg').selectAll("*").remove();
-    let svg = d3.select('#svg')
-        .attr("width", svgWidth)
-        .attr("height", svgHeight)
-
-    let g = svg.append('g')
-        .attr("transform", "translate(" + margin.left + "," + margin.top +")");
-
-    let x = d3.scaleTime()
-        .rangeRound([0, width]);
-        
-    let y = d3.scaleLinear()
-        .rangeRound([height, 0]);
-
-    // let line = d3.line()
-    // .x(function (d) {
-    //     console.log(d);
-    //     return x(d.date) 
-
-    // })
-
-    
-    // .y(function (d) { return y(d.value) })
-    //    x.domain(d3.extent(data, function (d) { return d.date })); y.domain(d3.extent(data, function (d) { return d.value }));
-
-
-    let line = d3
-        .line()
-        .x(function (d) {
-            // console.log(d);
-            { return x(d.date) }
+    console.log(data);
+    var ctx = document.getElementById("line-chart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            datasets: [{
+            label: 'Apple',
+            borderColor: '#7B1FA2',
+            backgroundColor: '#7B1FA2',
+            data: data.map((day) => (
+                {
+                    x: new Date(day.date),
+                    y: (day.close),
+                })),
+            }
+            ],
+        },
+        options: {
+            title: {
+                display: true,
+                text: "Chart.js Line Chart"
+            },
+            tooltips: {
+                mode: 'index',
+                intersect: false
+            },
+            responsive: true,
+            scales: {
+                xAxes: [{
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Week',
+                    },
+                    ticks: {
+                        source: 'data',
+                    },
+                    type: 'time',
+                    time: {
+                        unit: 'month',
+                        round: 'day',
+                        displayFormats: {
+                            month: 'MMM',
+                        },
+                    },
+                }],
+                yAxes: [{
+                    display: true,
+                    scaleLabel: {
+                        display: true,
+                        labelString: 'Value',
+                    },
+                    ticks: {
+                        beginAtZero: true,
+                    },
+                }],
+            },
+            title: {
+                display: true,
+                fontSize: 20,
+                text: `Stock Prices Past 5 days`,
+            },
         }
-        
-        )
-        .y(function (d) { return y(d.close) })   
-        x.domain(d3.extent(data, function (d) { return d.date })); 
-        y.domain(d3.extent(data, function (d) { return d.close }));
-    
-    g.append("g")
-        .attr("transform", "translate(0," + height + ")")
-        .call(d3.axisBottom(x))
-        .select(".domain")
-        .remove();
-
-    g.append("g")
-        .call(d3.axisLeft(y))
-        .append("text")
-        .attr("fill", "#000")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", "0.71em")
-        .attr("text-anchor", "end")
-        .text("Close ($)")
-        .style('fill', 'white');
-
-    g.append("path")
-        .datum(data)
-        .attr("fill", "none")
-        .attr("stroke", "lime")
-        .attr("stroke-linejoin", "round")
-        .attr("stroke-linecap", "round")
-        .attr("stroke-width", 1.5)
-        .attr("d", line);
-   
+    });
+    return myChart;
 }
 
 function parseCompanyData(data) {
-        const symbol = document.getElementById('symbol');
-        const exchange = document.getElementById('exchange');
-        const company = document.getElementById('company');
-        const site = document.getElementById('site');
-        const ceo = document.getElementById('ceo');
-        const sec = document.getElementById('sec');
-        const emp = document.getElementById('emp');
-        const st = document.getElementById('st');
-        const city = document.getElementById('city');
-    
-        symbol.innerHTML = "(" + data.symbol + ")";
-        exchange.innerHTML = data.exchange;
-        company.innerHTML = data.companyName;
-        site.innerHTML = data.website;
-        ceo.innerHTML = data.CEO;
-        sec.innerHTML = data.sector;
-        emp.innerHTML = data.employees;
-        add.innerHTML = data.address;
-        st.innerHTML = data.state;
-        city.innerHTML = data.city;
+    const symbol = document.getElementById('symbol');
+    const exchange = document.getElementById('exchange');
+    const company = document.getElementById('company');
+    const site = document.getElementById('site');
+    const ceo = document.getElementById('ceo');
+    const sec = document.getElementById('sec');
+    const emp = document.getElementById('emp');
+    const st = document.getElementById('st');
+    const city = document.getElementById('city');
+
+    symbol.innerHTML = "(" + data.symbol + ")";
+    exchange.innerHTML = data.exchange;
+    company.innerHTML = data.companyName;
+    site.innerHTML = data.website;
+    ceo.innerHTML = data.CEO;
+    sec.innerHTML = data.sector;
+    emp.innerHTML = data.employees;
+    add.innerHTML = data.address;
+    st.innerHTML = data.state;
+    city.innerHTML = data.city;
 };
 
 
